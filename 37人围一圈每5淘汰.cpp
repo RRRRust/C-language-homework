b127dd25e63b96b06f6896f62cc6df1bbe9c85f8
RRRRust <80036117+RRRRust@users.noreply.github.com>
2021-04-02 20:36:23 +0800

    Update 37人围一圈每5淘汰.cpp

--- 37人围一圈每5淘汰.cpp
+++ 37人围一圈每5淘汰.cpp
@@ -1,26 +1,28 @@
+//rrrrust
+//37人围成一圈，编上编号（1~37),第一个人从1数起，数到5的那个人被淘汰出局，接下来的那个人又开始从1数起，数到5的那个人也被淘汰……最后剩下的那个人为赢家，问第几个人是赢家？
 #include <stdio.h>
 
-#define nmax 50
+#define nmax 50  //最多50人玩
 
 int main()
 {
 	int i, k, m, n, num[nmax], * p;
 	printf("please input the total of numbers:");
-	scanf_s("%d", &n);
-	p = num;
+	scanf_s("%d", &n);  //人数
+	p = num;  //设置指针
 	for (i = 0; i < n; i++)
-		*(p + i) = i + 1;
+		*(p + i) = i + 1;  //循环编号
 	i = 0;
-	k = 0;
-	m = 0;
-	while (m < n - 1)
+	k = 0;  //读数器
+	m = 0;  //淘汰计数
+	while (m < n - 1)  //没有淘汰到最后一人就继续
 	{
 		if (*(p + i) != 0)
 			++k;
 		if (k == 5)
 		{
-			*(p + i) = 0;
-			k = 0;
+			*(p + i) = 0;  //被淘汰的人编号改为0
+			k = 0;   //重新开始的第一人就报1
 			++m;
 		}
 		++i;
